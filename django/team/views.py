c23225454a0832952a3525d6638c40a4aa6d42fb
dike.zhang <dike.zhang@gmail.com>
2016-10-22 12:08:49 -0400

    added coach

--- django/team/views.py
+++ django/team/views.py
@@ -190,6 +190,8 @@ class TeamHelper:
             logger.debug('helper user={0}'.format(player.number))
             if player.is_captain():
                 p['name'] = u''.join((last_name,first_name,'(',_('captain'),')')).encode('utf-8').strip()
+            elif player.is_coach():
+                p['name'] = u''.join((last_name,first_name,'(',_('coach'),')')).encode('utf-8').strip()
             else:
                 p['name'] = u''.join((last_name,first_name )).encode('utf-8').strip()
             p['id'] = player.id
