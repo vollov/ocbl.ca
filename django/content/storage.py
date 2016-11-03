656f8a41301b379f161cc836c12c3f3a2d21a9b3
dike.zhang <dike.zhang@gmail.com>
2016-11-02 20:42:05 -0400

    update file upload

--- django/content/storage.py
+++ django/content/storage.py
@@ -2,6 +2,9 @@ from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 import os
 
+import uuid, os, logging
+logger = logging.getLogger(__name__)
+
 class OverwriteStorage(FileSystemStorage):
 
     def get_available_name(self, name):
@@ -23,6 +26,9 @@ class OverwriteStorage(FileSystemStorage):
             super(MyModelName, self).save(*args, **kwargs)
         """
         # If the filename already exists, remove it as if it was a true file system
+        
+        logger.debug('handle uploaded file name ={0}', name)
         if self.exists(name):
+            logger.debug('remove existing file, name ={0}', name)
             os.remove(os.path.join(settings.MEDIA_ROOT, name))
         return name
--- django/content/views.py
+++ django/content/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from forms import UploadFileForm
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+def upload_photo(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
+def handle_uploaded_file(file):
+    print file.name
