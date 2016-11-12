d40f6ee7c5c43ed5fe9187068126f7078bcfc4a0
dike.zhang <dike.zhang@gmail.com>
2016-11-12 10:56:26 -0500

    ocbl settings

--- django/app/settings_prod.py
+++ django/app/settings_prod.py
@@ -1,4 +1,4 @@
 from settings import *
 
 DEBUG = CAPTCHA_TEST_MODE = TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ocbl.ca']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ocbl.ca', 'www.ocbl.ca']
